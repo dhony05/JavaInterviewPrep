dba8fd3f2e6bb2792d7bca01485156c9c83ad06c
Donelys Familia <donelysprivate@gmail.com>
2020-02-13 20:07:31 -0500

    fixing animal class

--- src/corejava/polymorphism/Animal.java
+++ src/corejava/polymorphism/Animal.java
@@ -3,12 +3,12 @@ package corejava.polymorphism;
 public class Animal {
 	
 	
-	public void sound(int i) {
-		System.out.println("sound");
-	}
+//	public void sound(int i) {
+//		System.out.println("sound");
+//	}
 
 	public void sound() {
-		// TODO Auto-generated method stub
+		System.out.println("sound");
 		
 	}
 	
