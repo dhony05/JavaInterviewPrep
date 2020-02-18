de034b087321a3115e41f5f3c4c28cdb3431254c
Donelys Familia <donelysprivate@gmail.com>
2020-02-17 20:45:46 -0500

    adding review

--- src/corejava/polymorphism/Animal.java
+++ src/corejava/polymorphism/Animal.java
@@ -3,9 +3,9 @@ package corejava.polymorphism;
 public class Animal {
 	
 	
-//	public void sound(int i) {
-//		System.out.println("sound");
-//	}
+	public void sound(int i) {
+		System.out.println("sound");
+	}
 
 	public void sound() {
 		System.out.println("sound");
