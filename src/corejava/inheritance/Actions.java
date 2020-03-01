08aa012e8a94de61549c89c315cc0f92744fe333
Donelys Familia <donelysprivate@gmail.com>
2020-02-29 23:34:40 -0500

    defined functions

--- src/corejava/inheritance/Actions.java
+++ src/corejava/inheritance/Actions.java
@@ -2,7 +2,11 @@ package corejava.inheritance;
 
 public abstract class Actions {
 
-	
+/***
+ * This method will turn to sleep the person
+ * 
+ * @param person
+ */
  public static  void sleep(person person) {
 	 System.out.println(person.name + " is sleeping");
  }
--- src/corejava/inheritance/TV.java
+++ src/corejava/inheritance/TV.java
@@ -2,10 +2,21 @@ package corejava.inheritance;
 
 public interface TV {
 	
-	
+	/***
+	 * This abstract method will be overridden to switch the channel 
+	 * @param channel
+	 * 
+	 */
 	abstract void switchChannel(int channel);
 	
+	/***
+	 * implementing turning on the TV
+	 */
 	abstract void turningTVOn();
+	
+	/***
+	 * Implementing turning off the TV
+	 */
 	
 	abstract void turningTVOff();
 
