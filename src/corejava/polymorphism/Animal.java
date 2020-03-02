1035e832a0d2d5edb9c2478176e71c6d6db9725d
Donelys Familia <donelysprivate@gmail.com>
2020-03-01 20:19:26 -0500

    methods overloaded added

--- src/corejava/polymorphism/Animal.java
+++ src/corejava/polymorphism/Animal.java
@@ -2,10 +2,10 @@ package corejava.polymorphism;
 
 public class Animal {
 	
-	
-	public void sound(int i) {
-		System.out.println("sound");
-	}
+//	
+//	public void sound(int i) {
+//		System.out.println("sound");
+//	}
 
 	public void sound() {
 		System.out.println("sound");
